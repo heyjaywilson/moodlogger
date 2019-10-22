984c65e8641b8d6f1dda89740b423d8e855d7659
Maegan Wilson <maegan@maeganwilson.com>
2019-10-21 19:18:29 -0500

    It’s working for adding moods, but I need to not add duplicate logs

--- mood-log/mood-log/views/LogViews/NewLog.swift
+++ mood-log/mood-log/views/LogViews/NewLog.swift
@@ -19,22 +19,35 @@ struct NewLog: View {
     }
     
     @State private var logDate = Date()
+    @State private var chosenMood: Int = 0
+    private var moods: [String] = ["😀", "😁", "😂", "🤣", "😃", "😍", "🥰", "😴", "🤩", "🙃"]
     
     var body: some View {
         NavigationView {
             Form {
-                DatePicker(selection: $logDate, displayedComponents: .date) {
-                    Text("Log Date")
+                Section{
+                    DatePicker(selection: $logDate, displayedComponents: .date) {
+                        Text("Log Date")
+                    }
+                    MoodPicker(chosenMood: $chosenMood)
+                }
+                Button(action:{
+                    self.saveLog()
+                }){
+                    Text("Save")
                 }
             }
-            .navigationBarTitle("New Note")
+            .navigationBarTitle("New Log")
             .navigationBarItems(trailing:
                 Button(action: {
-                    self.saveLog()
+                    self.cancelLog()
                 }) {
-                    Text("Save")
+                    Text("Cancel")
             })
         }
+    }
+    func cancelLog(){
+        presentationMode.wrappedValue.dismiss()
     }
     
     func saveLog() {
@@ -42,6 +55,12 @@ struct NewLog: View {
         newLog.date = logDate
         newLog.id = UUID()
 
+        let newMood: MoodEntity = MoodEntity(context: context)
+        newMood.date = logDate
+        newMood.id = UUID()
+        newMood.mood = moods[chosenMood]
+        newMood.log = newLog
+        
         print(newLog)
         do {
             print(newLog)
@@ -49,7 +68,7 @@ struct NewLog: View {
         } catch {
             print(error)
         }
-
+        
         presentationMode.wrappedValue.dismiss()
     }
 }
