699875511c4e50afa9fd7d565e206a2cab9b0b61
Maegan Wilson <maegan@maeganwilson.com>
2020-01-31 07:56:59 -0500

    Working on showing moods

---
+++ mood-log/mood-log/views/log views/row views/Moods.swift
@@ -0,0 +1,21 @@
+//
+//  Moods.swift
+//  mood-log
+//
+//  Created by Maegan Wilson on 1/31/20.
+//  Copyright © 2020 Maegan Wilson. All rights reserved.
+//
+
+import SwiftUI
+
+struct Moods: View {
+  var body: some View {
+    Text("Hello, World!")
+  }
+}
+
+struct Moods_Previews: PreviewProvider {
+  static var previews: some View {
+    Moods()
+  }
+}
